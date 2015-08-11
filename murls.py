f93b9ba127f6625ec2720cfdc328d1a2ccad1d23
cyberbikepunk <loic@cyberpunk.bike>
2015-08-11 22:21:39 +0200

    The API now works but has no bells and whistles yet.

--- murls.py
+++ murls.py
@@ -1,4 +1,4 @@
-""" murls: mutable URL strings. """
+""" murls (Mutable URL Strings): an expressive way to manipulate URLs. """
 
 from collections import UserString
 
@@ -6,45 +6,91 @@ from collections import UserString
 class Url(UserString):
     """ The base class for Mutable URL Strings. """
 
+    _specs = ('{schema}://'
+              '{username}{colon1}{password}{at}'
+              '{host}{colon2}{port}'
+              '{forward_slash}{path}'
+              '{question_mark}{query}'
+              '{hash}{fragment}')
+
     def __init__(self, *args, **kwargs):
         super(Url, self).__init__(*args, **kwargs)
 
-        self._scheme = ''
-        self._username = ''
-        self._password = ''
-        self._domain = args[0]
-        self._host = ''
-        self._ip = ''
-        self._port = ''
-        self._path = ''
-        self._query = ''
-        self._fragment = ''
+        self._schema = str()
+        self._host = str()
+        self._path = list()
+        self._query = dict()
+        self._fragment = str()
 
-    def __contains__(self, item):
-        return item in self.data
+        # Not yet implemented
+        self._username = str()
+        self._password = str()
+        self._port = 0
 
-    def path(self, *tree):
-        self._path = '/'.join(tree)
+    def host(self, host):
+        self._host = host
+        self.data = self._build()
+        return self
+
+    def schema(self, schema):
+        self._schema = schema
+        self.data = self._build()
+        return self
+
+    def fragment(self, fragment):
+        self._fragment = fragment
+        self.data = self._build()
+        return self
+
+    def path(self, *path):
+        self._path = path
         self.data = self._build()
         return self
 
     def query(self, **parameters):
-        self._query = parameters
+        self._query.update(parameters)
         self.data = self._build()
         return self
 
-    def port(self):
-        pass
+    @property
+    def _parts(self):
+        return {'schema': self._schema,
+                'username': self._username,
+                'colon1': self._colon1,
+                'password': self._password,
+                'host': self._host,
+                'colon2': self._colon2,
+                'port': str(self._port) if self._port else '',
+                'forward_slash': self._forward_slash,
+                'path': '/'.join(self._path),
+                'question_mark': self._question_mark,
+                'query': '&'.join([str(k) + '=' + str(v) for k, v in self._query.items()]) if self._query else '',
+                'hash': self._hash,
+                'fragment': self._fragment,
+                'at': self._at}
 
-    def username(self):
-        pass
+    def _build(self):
+        return self._specs.format(**self._parts)
 
-    def password(self):
-        pass
+    @staticmethod
+    def port(port):
+        raise NotImplemented('Cannot insert port %s' % port)
+
+    @staticmethod
+    def username(username):
+        raise NotImplemented('Cannot insert username %s' % username)
+
+    @staticmethod
+    def password(password):
+        raise NotImplemented('Cannot insert password %s' % password)
 
     @property
-    def colon(self):
+    def _colon1(self):
         return ':' if self._password else ''
+
+    @property
+    def _colon2(self):
+        return ':' if self._port else ''
 
     @property
     def _at(self):
@@ -52,24 +98,32 @@ class Url(UserString):
 
     @property
     def _hash(self):
-        return '#' if self._query else ''
+        return '#' if self._fragment else ''
 
     @property
     def _question_mark(self):
         return '?' if self._query else ''
 
     @property
-    def _end_slash(self):
+    def _forward_slash(self):
         return '/' if self._path else ''
 
-    def _build(self):
-        return self._scheme + self._host + self._end_slash + self._path + self._question_mark + self._query + self._hash + self._fragment
+    @property
+    def _left_bracket(self):
+        return '[' if self._username else ''
+
+    @property
+    def _right_bracket(self):
+        return ']' if self._username else ''
+
+    def __contains__(self, item):
+        return item in self.data
 
 
 class Http(Url):
     def __init__(self, *args, **kwargs):
         super(Http, self).__init__(*args, **kwargs)
-        self._scheme = 'http://'
+        self._schema = 'http'
         self._host = args[0]
         self.data = self._build()
 
@@ -77,7 +131,7 @@ class Http(Url):
 class Https(Url):
     def __init__(self, *args, **kwargs):
         super(Https, self).__init__(*args, **kwargs)
-        self._scheme = 'https://'
+        self._schema = 'https'
         self._host = args[0]
         self.data = self._build()
 
@@ -91,4 +145,5 @@ def https(site):
 
 
 if __name__ == '__main__':
-    pass
+    url = http('cdkjkljbelkvbfl').path('jknjnk').query(foo='bar')
+    print(url)
